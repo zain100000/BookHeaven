d20066ebaf479d7833e9891eae119c839b91dc93
zain100000 <muhammadzainulabideen292@gmail.com>
2025-04-10 00:04:22 +0500

    modify the backend url

--- book_heaven_super_admin/src/redux/config/Config.jsx
+++ book_heaven_super_admin/src/redux/config/Config.jsx
@@ -1,6 +1,6 @@
 const CONFIG = {
-  // BACKEND_API_URL: "http://localhost:8000/api",
-  BACKEND_API_URL: "https://book-heaven-backend-deployment.onrender.com/api",
+  BACKEND_API_URL: "http://localhost:8000/api",
+  // BACKEND_API_URL: "https://book-heaven-backend-deployment.onrender.com/api",
 };
 
 export default CONFIG;
